a6fb46606eb3c0d3cf6c0c0d1b235296bc0495be
Ivanov_SV <Ivanov_SV3@yndex.ru>
2022-01-24 19:29:24 +0300

    Add project

---
+++ PZ_05/PZ_05.py
@@ -0,0 +1,8 @@
+# Написать программу вычисления значения функции y = f(a)
+import numbers
+from tkinter import Y
+
+
+number = int(input("ведите значение аргумента функции а   "))
+
+print(f'значение функции у =  {number*8 + 4}')
