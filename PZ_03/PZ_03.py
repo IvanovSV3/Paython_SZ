ed592c3186089ab373024a2fa6cb4bd307166926
Ivanov_SV <Ivanov_SV3@yndex.ru>
2022-01-24 19:17:47 +0300

    Add project

---
+++ PZ_03/PZ_03.py
@@ -0,0 +1,16 @@
+# 3 По заданному номеру дня недели вывести его название
+number = int(input("введите число от 1 до 7  "))
+if number == 1:
+    print('понедельник')
+if number == 2:
+    print('вторник')
+if number == 3:
+    print('среда')
+if number == 4:
+    print('четверг')
+if number == 5:
+    print('пятница')
+if number == 6:
+    print('субота')
+if number == 7:
+    print('воскресенье')
